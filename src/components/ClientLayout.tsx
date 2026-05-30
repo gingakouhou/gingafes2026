d6b33ec9b010879f5c02f3b6fb130b009276a268
numa <dazhaokong975@gmail.com>
2026-05-30 10:08:02 +0900

    refactor: 黒いオープニング画面（EntranceGate）を削除

--- src/components/ClientLayout.tsx
+++ src/components/ClientLayout.tsx
@@ -1,17 +1,11 @@
 "use client";
 
 import { ReactNode } from "react";
-import Splash from "@/components/Splash";
 
 interface ClientLayoutProps {
   children: ReactNode;
 }
 
 export default function ClientLayout({ children }: ClientLayoutProps) {
-  return (
-    <>
-      <Splash />
-      {children}
-    </>
-  );
+  return <>{children}</>;
 }
